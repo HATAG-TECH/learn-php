b95568203872ae80d0c0297fbc5a5a41c96d3323
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-03 10:57:43 -0800

    add form submission

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+
+        <label for="quantity">quantity</label>
+        <input type="text" name="quantity" id="quantity">
+        <input type="submit" value="total">
+    </form>
+</body>
+</html>
 <?php
 echo "PHP is working!<br>";
 
 $name = "Hatag";
 $age = 20;
+$price=30;
 $country = "Ethiopia";
-
+$quantity=$_GET["quantity"];
+$total=$price*$quantity;
+echo "total is:{$total}";
 echo "My name is " . $name . "<br>";
 echo "I am " . $age . " years old.<br>";
 echo "I live in " . $country;
