e3fda33a629b9093c89f99c743006b6c067b89b8
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-03 10:36:49 -0800

    first program

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "PHP is working!<br>";
+
+$name = "Hatag";
+$age = 20;
+$country = "Ethiopia";
+
+echo "My name is " . $name . "<br>";
+echo "I am " . $age . " years old.<br>";
+echo "I live in " . $country;
+?>
