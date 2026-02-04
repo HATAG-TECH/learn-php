a125cbf883006bcf682abda3c99143b48bad3f93
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-03 23:58:54 -0800

    Comparison Operators operator practice

---
+++ operators/Comparison Operators.php
@@ -0,0 +1,10 @@
+<?php
+$a = 10;
+$b = 5;
+
+var_dump($a > $b);   // true
+var_dump($a < $b);   // false
+var_dump($a == 10);  // true (equal)
+var_dump($a != $b);  // true (not equal)
+var_dump($a === 10); // true (equal value & type)
+?>
