3f8a333fc94c0de0876e6d74f699ccaadee4a465
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-03 23:55:23 -0800

    arthimetic operator practice

---
+++ operators/arthimetic.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+echo $a + $b;  // Addition: 15
+echo "<br>";
+
+echo $a - $b;  // Subtraction: 5
+echo "<br>";
+
+echo $a * $b;  // Multiplication: 50
+echo "<br>";
+
+echo $a / $b;  // Division: 2
+echo "<br>";
+
+echo $a % $b;  // Modulus (remainder): 0
+?>
