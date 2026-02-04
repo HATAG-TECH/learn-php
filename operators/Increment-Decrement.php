a10840001d09b709f7460f4cacb0f9db9592ade9
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-03 23:57:10 -0800

    increement decrement  operator practice

---
+++ operators/Increment-Decrement.php
@@ -0,0 +1,14 @@
+<?php
+$x = 5;
+
+echo ++$x;  // Pre-increment: 6
+echo "<br>";
+
+echo $x++;  // Post-increment: prints 6, then becomes 7
+echo "<br>";
+
+echo --$x;  // Pre-decrement: 6
+echo "<br>";
+
+echo $x--;  // Post-decrement: prints 6, then becomes 5
+?>
