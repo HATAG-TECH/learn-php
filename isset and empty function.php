a56700bdc608b7ed1156f049ed646906ee1b8a6b
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-07 10:38:48 -0800

    by using username and password form of html functions methods

--- isset and empty function.php
+++ isset and empty function.php
@@ -6,12 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="array.php" method="post" >
-        <label for="x">x:</label>
-        <input type="text" name="x" id="x"> <br>
-         <label for="y">y:</label>
-        <input type="text" name="y" id="y">
-        <input type="submit" value="total">
+    <form action="isset and empty function.php" method="post" >
+        <label for="username">username:</label>
+        <input type="text" name="username" id="username"> <br>
+         <label for="password">password:</label>
+        <input type="text" name="password" id="password">
+        <input type="submit" value="log in">
     </form>
 </body>
 </html>
+<?php
+$username= $_POST["username"];
+$password= $_POST["password"];
+echo "Username: {$username} <br>";
+echo "Password: {$password} <br>";
+
+?>
