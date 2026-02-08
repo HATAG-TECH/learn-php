47d9e992b3152cab5dcee7b0d0d3ca0d221e0519
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-07 23:36:07 -0800

    improve the form or username annd password input and display way

--- isset and empty function.php
+++ isset and empty function.php
@@ -11,14 +11,39 @@
         <input type="text" name="username" id="username"> <br>
          <label for="password">password:</label>
         <input type="text" name="password" id="password">
-        <input type="submit" value="log in">
+        <input type="submit" name="login" value="log in">
     </form>
 </body>
 </html>
 <?php
-$username= $_POST["username"];
-$password= $_POST["password"];
-echo "Username: {$username} <br>";
-echo "Password: {$password} <br>";
+// $username= $_POST["username"];
+// $password= $_POST["password"];
+// echo "Username: {$username} <br>";
+// echo "Password: {$password} <br>";
+
+
+
+// foreach($_POST as $key => $value){
+//     echo "{$key} = {$value} <br>";
+// }
+
+if(isset($_POST["login"])){
+    $username= $_POST["username"];
+    $password= $_POST["password"];
+    if(empty($username)){
+        echo "username is missing";
+    }
+    elseif(empty($password)){
+         echo "password is missing";
+
+    }
+    else{
+        echo "username and password is inserted <br>";
+        echo "username: {$username} <br>";
+        echo "password: {$password} <br>";
+    
+    }
+}
+
 
 ?>
