1df72d804d6f4abbae495f24f476287e449df341
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-26 09:41:58 -0800

    practice functions

--- function/basic function.php
+++ function/basic function.php
@@ -1,7 +1,19 @@
+// basic function
 <?php
 function sayHello(){
     echo "Hello Hatag!";
 }
 
 sayHello(); // calling the function
+?>
+
+// function with parameter
+
+<?php
+function greet($name){
+    echo "Hello " . $name . "<br>";
+}
+
+greet("Hatag");
+greet("John");
 ?>
