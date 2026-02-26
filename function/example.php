e4791fc3327fb5bf4af61f618860411297fedae8
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-02-26 09:50:38 -0800

    practice functions example of function with form input

--- function/example.php
+++ function/example.php
@@ -7,3 +7,28 @@ function calculateTotal($price, $quantity){
 $total = calculateTotal(100, 3);
 echo "Total: " . $total;
 ?>
+
+// example of function with form input
+<?php
+function calculateTotal($price, $quantity){
+    return $price * $quantity;
+}
+
+if(isset($_POST["submit"])){
+    $price = $_POST["price"];
+    $quantity = $_POST["quantity"];
+    $total = calculateTotal($price, $quantity);
+}
+?>
+
+<form method="post">
+    Price: <input type="number" name="price"><br>
+    Quantity: <input type="number" name="quantity"><br>
+    <input type="submit" name="submit">
+</form>
+
+<?php
+if(isset($total)){
+    echo "Total: " . $total;
+}
+?>
