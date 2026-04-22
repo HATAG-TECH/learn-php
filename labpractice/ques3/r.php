201b838599f9619216319db190df80121657a6dd
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-04-22 10:44:26 -0700

    Q3 split part 1: remove combined file, add r and w examples

---
+++ labpractice/ques3/r.php
@@ -0,0 +1,13 @@
+<?php
+// Mode: r (read only - writing is not allowed)
+$data = ["id" => 1, "name" => "Aster", "role" => "Developer"];
+$json = json_encode($data, JSON_PRETTY_PRINT);
+$file = fopen(__DIR__ . "/user.json", "r");
+
+if ($file) {
+    echo "Mode r: File opened in read-only mode. Writing with fwrite() is not allowed.";
+    fclose($file);
+} else {
+    echo "Mode r: Could not open user.json. The file may not exist.";
+}
+?>
