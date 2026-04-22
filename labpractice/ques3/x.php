7e46335a5a84cc8aee611b68807e5604afb3a3ad
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-04-22 10:44:51 -0700

    Q3 split part 4: add x and x+ examples

---
+++ labpractice/ques3/x.php
@@ -0,0 +1,14 @@
+<?php
+// Mode: x (write - create new file, fail if file exists)
+$data = ["id" => 1, "name" => "Aster", "role" => "Developer"];
+$json = json_encode($data, JSON_PRETTY_PRINT);
+$file = @fopen(__DIR__ . "/user.json", "x");
+
+if ($file) {
+    fwrite($file, $json);
+    fclose($file);
+    echo "Mode x: New file created and data written.";
+} else {
+    echo "Mode x: Failed. user.json already exists.";
+}
+?>
